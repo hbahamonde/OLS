06b230b0c675e84628319f04f7d18f02a40829b0
Hector <bahamondeh@gmail.com>
2021-07-08 16:28:29 -0400

    problem set

--- Tests/Problem_Sets/PS3.R
+++ Tests/Problem_Sets/PS3.R
@@ -1,6 +1,5 @@
 # Prof. Héctor Bahamonde.
 # Ayudante: Gonzalo Barría.
-# Fecha entrega: una semana de plazo desde su fecha de publicacion en uCampus (Tareas).
 
 cat("\014")
 rm(list=ls())
@@ -16,7 +15,6 @@ library(foreign)
 dat = read.csv("https://github.com/hbahamonde/OLS/raw/master/Datasets/saurus.csv", header=F) 
 
 
-
 # En este ej tendras la oportunidad de trabajar en dos bases de datos ("anscombe" y "dat"). Armado/a con todo lo que tengas para analizar un modelo, quiero que veas si los cinco modelos que estimaras cumplen con los supuestos de la regresion lineal (Gauss-Markov). 
 
 # Usa todas las herramientas que TU creas convenientes!
@@ -30,19 +28,19 @@ dat = read.csv("https://github.com/hbahamonde/OLS/raw/master/Datasets/saurus.csv
 # e. Las X's no son invariables, es decir, las X's son variables, no constantes?
 
 
-# 1. Modelos
+# Modelos
 
-### 1.1 Usando la base "anscombe", estima los siguientes modelos usando el comando "lm"
+### Usando la base "anscombe", estima los siguientes modelos usando el comando "lm"
 ## modelo.1 = y1 ~ x1
 ## modelo.2 = y2 ~ x1
 ## modelo.3 = y3 ~ x1
 ## modelo.4 = y4 ~ x4
 
-### 1.2 Usando la base "dat", estima el siguientee modelo usando el comando "lm"
+### Usando la base "dat", estima el siguientee modelo usando el comando "lm"
 ## modelo.5 = V2 ~ V1
 
 
-# 2. Muestra tus resultados en una tabla. 
+# Muestra tus resultados en una tabla. 
 # library(texreg)
 # screenreg(list(modelo.1, modelo.2, modelo.3, modelo.4, modelo.5))
 
