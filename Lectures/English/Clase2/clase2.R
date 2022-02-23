db534b8389c5c67eb05e005f03c6a6a8f194aed5
Hector <bahamondeh@gmail.com>
2022-02-23 18:07:23 +0200

    ready

--- Lectures/English/Clase2/clase2.R
+++ Lectures/English/Clase2/clase2.R
@@ -1,17 +1,17 @@
-# Funciones básicas: promedio, help(), operadores, tipos de objetos (character, arrays, fechas, listas, dataframes).
+# Basis functions: mean, help(), operators, objects (character, arrays, dates, lists and data.frames)
 
 
 cat("\014")
 rm(list=ls())
 
 # 1+2
-## Esta linea es un comentario. No ejecuta nada. 
-## Destacala, y aprieta "Run" (que significa, "correr").
-## Solo aparece en la consola, sin hacer nada.
+## This line is a "comment." It doesn't do anything at all.
+## Now, highlight it, and run it ("Run" button) . 
+## It will just show in the Console without doing anything.
 
-## En cambio si escribo sin comentario lo siguiente:
+## However, if you remove the comment, the next line will be run 
+## Now you run the following lines. 
 3+3
-## y aprieto "Run", se ejecuta el resultado.
 
 10/2
 
@@ -20,139 +20,173 @@ rm(list=ls())
 (3+3)/2
 
 ##################
-## paquetes
+## Packages
 ##################
 
-## Instalando paquetes
-install.packages("ggplot2") # Esta es la libreria de graficos mas potentes que existe. 
-library(ggplot2)
+## Installing Packages
+install.packages("ggplot2") # This line installs a "library" called "ggplot2" that makes plots. 
+library(ggplot2) # And this line "calls" this library so we can use it.
+## NOTE: If you want to use a library/package, you need to make sure you have it installed first.
+## If you do, then you just need to call the library as in "library(ggplot2)".
 
-
-### Anda a "Packages", y busca la libreria que acabamos de instalar. 
-### En el buscador, escribe "ggplot2", y comprueba que hay un ticket al lado de "ggplot2".
-### Ahora, desinstala la librería, des-tickeando "ggplot2".
-### Lo que acabas de hacer es "desatachar" la libreria. 
-### Ahora, atachala nuevamente. Fijate que la consola dice "library("ggplot2"). 
+## There is also another way to do the same.
+## Go to "Packages" and search for "ggplot2"
+## If the package is installed, checking the library replaces "library(ggplot2)"
+## Now, let's detach the library (by unchecking), and then uninstall the whole library ("x").
 
 
 ##################
-## Ayuda, y otras funciones
+## Asking R for help
 ##################
 
-# Problema: necesitas crear una secuencia de numeros, por ej. del 1 al 10. Ocupa la funcion "seq". Pero no sabes ocuparla.
+# Issue: you need to create a sequence of numbers, for instance, from 1 to 10.
+# Maybe a good idea is to assume that our function might be called (or contain)
+# the word "seq". You actually don't know the name of the package/function.
+# Let's ask for help...
 help(seq)
-help(secuencia)
+help(sequence)
 
-# Ahora, la pestana "help" se abre. Lee el menu de ayuda.
-# Ahora, escribamos la funcion.
+# Now we know that there are actually two packages that might do the job.
+# "seq" and "sequence".
+
+# Now, as you can see, the "Help" tab will open. Let's read that...
+# Ok, now that we know how to use the "seq" function, let's use it.
+
 seq(from = 1, to = 10)
 
-## Atencion: fijate en la coma en cada parte del argumento.
+# Note that there is a comma between "arguments" (internal parts that this function accepts).
 
-# Supongamos que necesitabamos esta freq. cada dos numeros. Usemos el argumento "by"
+# Now, lets suppose we need a sequence by two's. Let's use the argument "by".
+
 seq(from = 1, to = 10, by=2)
 
-# Y de mayor a menor?
+# How about a decreasing sequence, by 2?
 seq(10, 1, -2)
-## Fijate que tuvimos que especificar que la secuencia va de 10 a 1, en vez de 1 a 10.
 
-# Esas eran secuencias. Ahora veamos repeticiones.
-## help(seq) asume que tu sabes que la funcion se llama "seq". Que pasa si no tienes idea?
-## Pidamos ayuda en general...Lo que necesitamos es "repeat", o "repetition", o "repeated"....Sabemos que empieza con "rep"
+# Notice that the sequence goes "from" the largest value "to" the smallest value.
+# Notice also that we did not specify "from" nor "to". 
+# As long as you preserve the order of the arguments, the package will "know" what
+# you're talking about.
+
+# OK. Enough with sequences. Let's see "repetitions."
+# "help(seq)" assumes that you know that the function should be called (more or less)
+# "seq". However, what if you have no clue about the name of the function?
+# For sure we know that the first place to start looking for "repetitions" should
+# contain "rep," for "repeat" or "repetition" or "repeated"...
+
+# Let's do a more general search...
 ??rep
-## Ahora encuentra lo que buscas ("Replicate Elements of Vectors and Lists"). Es de la "base" de R (la estructura basica de R, es decir, que no es un paquete aparte). Click on it. Hemos accedido a la ayuda de la funcion "rep". Repitamos algo importante 100 veces.
-rep("esta es la mejor clase del semestre", 100)
+# Now let's look for "Replicate Elements of Vectors and Lists". This is in the "base" of R (the core and basic R functions that come with it).
+# Now, let's use this function, and repeat something...
+rep("May the force be with you", 100)
 
-
-# combinemos las funciones seq y rep. Usemos una estructura "anidada" (i.e. una funcion dentro de otra).
+# Let's try to combine "seq" and "rep" by using a "nesting coding structure" (a function within another function).
 rep(seq(1, 4, 1), 5)
 
-
 ##################
-## Objetos
+## Objects
 ##################
 
-# La ventaja de R es que esta orientado a "objetos". Creemos uno.
+# One of the many advantages of R is that it is an "object-oriented" piece of software.
+# Unlike state, for instance, you can store in your memory several datasets, numbers, strings, etc. Stata, for instance, can only handle one dataset at a time.
+# Let's create an object.
 abc <- 3
 
-# Aqui, acabamos de crear el objeto "abc", al que le hemos asignado un numero (el 3).
-# Ahora "llamemos" el objeto "abc"
+# We've just created an object called "abc" which has a number "3" inside it.
+# Now, let's call the "abc" object...
 abc
-## Fijate que en la consola aparece "[1] 3". Eso significa que solo hay una linea "[1]", que contiene el objeto 3.
-## Ahora creemos un objeto, pero que en vez de contener un numero, contenga texto.
-tres <- "cuatro"
-## Llamemos al objeto "tres"
-tres
-## Fijate que el texto entra como texto cada vez que le ponemos comillas. Mira:
-tres <- "3"
-## Ahora 3 es un texto, en vez de un numero. Llamalo.
 
-## Lo importante, es que nosotros podemos trabajar con estos objetos. Recuerda que el objeto "abc" contiene el numero tres. Mira:
-abc + abc
-## Acabamos de sumar el objeto "abc". Ahora tratemos de elevar el objeto "abc" al cuadrado, y guardar ese resultado (9) en otro objeto.
-abc.al.cuadrado <- abc^2
-## Llama al objeto "abc.al.cuadrado"
-abc.al.cuadrado
+# Notice that the Console shows "[1] 3". This means that there is only one line, hence "[1]".
 
-## Ahora, revisa el "Environment" (el medio ambiente) que creaste. Esa ventana contiene todos los objetos que creaste. Revisa que tienes:
+# Now let's create an object that contains text, or "characters."
+
+three <- "four"
+
+# Now let's call the object "three"...
+three
+
+# Notice that characters always go with quotation marks... ""
+# Let's take advantage of this situation, and let's transform the number 3 in text
+three <- "3"
+
+# Now "3" is text instead of a number. Let's (try to) use it...
+three + three
+
+# Ups...something went really wrong...
+
+# Let's fix it..
+three <- 3
+
+three + three
+
+# Let's keep trying different alternatives...
+
+three.sq <- three^2
+
+three.sq
+
+# Let's check the "Environment". This window contains all the things we've created.
 ls()
 
-## borra todo lo que creaste.
+# Let's delete (remove, "rm") them all.
 rm(list=ls())
 
-## revisa nuevamente que tienes...
-ls() # nada.
+# And let's check everything is empty...
+ls() # nothing to see here...
 
 
 ##################
-## Tipos de objetos: Vectores
+## Other objects: Vectors
 ##################
 
-# Vector numerico
+# Numeric vector
 vector.n = c(1,2,3,4)
 vector.n
-## En este vector, hemos incluido 4 numeros. Es un vector numerico.
+## In this vector, we've included four numbers. It's a numeric vector.
 
-# Vector de texto
-vector.t = c("uno", "dos", "tres", "cuatro")
+# Vector of text
+vector.t = c("one", "two", "three", "four")
 vector.t
 
-# Multiplequemos por dos el vector numerico
+# Let's multiply some vectors...[represent this in whiteboard].
 vector.n.2 = vector.n*2
 vector.n.2
 
-# Comprobemos que vector.n.2 > vector.n
+# Let's check vector.n.2 > vector.n
 vector.n.2 > vector.n
 
-# acceder al segundo elemento de un vector
+# Let's check the second elements in our vector...
 vector.n.2[2]
 
 
 ##################
-## Tipos de objetos: Listas
+## Other objects: Lists
 ##################
 
-# creemos una lista, que es un objeto que permite una variedad de elementos dentro de ella.
-mi.informacion <- list(
-  clases=c("Contabilidad General", "Metodos", "Estadistica I"),
-  profes=c("X", "HB", "Y"),
-  WhatsApp="+569475759"
-  )
+# let's create a list. this type of object is really flexible and allows us having different kinds of elements inside of it.
 
-# accedamos a un elemento por posicion "2" (profes)
-mi.informacion[[2]]
+my.info <- list(
+  class=c("Intro to Epistemology", "Quant Methods", "Political Theory"),
+  prof=c("X", "HB", "Y"),
+  WhatsApp="+358-123456789"
+)
 
-# accedamos a un elemento por nombre
-mi.informacion$clases
+# lets check the second element of the list (prof)
+my.info[[2]] # check double square-brackets!
 
-# accedamos a un elemento por nombre y posicion
-mi.informacion$profe[2]
+# lets check now but using the subclass, 
+my.info$class
+
+# lets check now the second element of the second subclass 
+my.info$prof[2]
+
+# Notice that the lenght of each subclass may be different!
 
 ##################
-## Tipos de objetos: Matrix
+## Other objects: Matrix
 ##################
 
-# creemos una matriz, cuyos escalares (numeros) sigan una secuencia del 1 al 6, 1:6, y que tenga dos filas (rows).
+# let's create a matrix whose scalars (fancy word for "number" in the matrix algebra jargon) follow a sequence from 1 to 6, and put them in two rows.
 a <- matrix(1:6 , nrow=2)
 a
 
@@ -166,72 +200,61 @@ a[ ,2]
 a[1,]
 
 ##################
-## Tipos de objetos: Fechas
+## Other objects: Dates
 ##################
 
 a = as.Date('1/15/2001',format='%m/%d/%Y')
 b = as.Date('1/16/2001',format='%m/%d/%Y')
 c = as.Date('1/17/2001',format='%m/%d/%Y')
 
-# ahora R entiende que una cantidad sea mayor (mas reciente) que otra (mas antigua)
+# Now R "understands" that some dates are larger (older!) than others ("more recent"). 
 a>b
 
-# crear texto
-cualquier.cosa = "2001-01-17"
-
-c==cualquier.cosa # compara ambos textos (que son iguales).
-c>cualquier.cosa # como detecta que ambos son iguales, esta correcto al establecer que una cantidad NO ES mayor a otra.
-# Sin embargo, siempre es mejor establecer el tipo de objeto que queremos (en este caso, una fecha).
-
 ##################
-## Tipos de objetos: Dataframes (bases de datos)
+## Other objects: data.frame
 ##################
 
 
-base.de.datos <- data.frame(
-        diabetico = c(TRUE, FALSE, TRUE, FALSE), 
-        kilos = c(65, 69, 71, 73),
-        nombre = c("Pedro", "Juan", 'Diego', "Maria")
-        )
+dataset <- data.frame(
+  diabetic = c(TRUE, FALSE, TRUE, FALSE), 
+  kilos = c(65, 69, 71, 73),
+  name = c("A", "B", 'C', "D")
+)
 
-# describir lo basico de un df: head
-head(base.de.datos)
+# let's describe "the head" of the dataset
+head(dataset)
 
-# describir lo basico de un df: "View"
-View(base.de.datos) # Igual a un Excel
+# let's inspect the dataset using something like Excel
+View(dataset)
 
-base.de.datos[3,2] # fila 3, columna 2
+dataset[3,2] # row 3, column 2
 
-base.de.datos[1:2, "kilos"] # filtrando, pero usando el nombre de la columna (en vez del numero de columna). Para las filas, la secuencia de los dos primeros elementos. 
+dataset[1:2, "kilos"] # Let's filter using the name of the column instead of the number. For the rows we will use the sequence of the first two elements. 
 
-base.de.datos[ ,"diabetico"] # todas las filas de la columna "diabetico"
+dataset[ ,"diabetic"] # let's filter all rows for all observations in the "diabetic" column.
 
-## Nombres de las Columnas
-colnames(base.de.datos)
+## let's inspect the column names of the data.frame
+colnames(dataset)
 
-## Acabas de descubrir un error. No eran kilos, si no que "edad". Cambia el nombre de las columnas.
-colnames(base.de.datos) <- c("diabetico", "edad", "nombre")
+## Ups, let's pretend you have just discovered it wasn't "kilos" but "age". Let's change the name of that column.
+colnames(dataset) <- c("diabetic", "age", "name")
 
-## Comprueba...
-base.de.datos
-colnames(base.de.datos)
+## Let's check...
+dataset
+colnames(dataset)
 
-## Resumen del df
-summary(base.de.datos)
+## Summary of the df
+summary(dataset)
 
-## Prestige
-install.packages("car") # instalemos la base, que viene en la libreria "car"
-library(car) # ahora, carguemos la base que viene en la libreria "car"
+## Now let's call the "Prestige" data
+install.packages("car") # install the dataset that lives in the "car" library.
+library(car) # now let's load the dataset
 
-## Veamos como se ve toda la base
+## Let's insepct the dataset.
 View(Prestige)
-
-## Breve inspeccion de la base
 head(Prestige)
 
-
-
-## Resumen de la base
+## Let's summarize the dataset.
 summary(Prestige)
 
 ### education: Average education of occupational incumbents, years, in 1971.
@@ -242,15 +265,15 @@ summary(Prestige)
 ### type: Type of occupation. A factor with levels (note: out of order): bc, Blue Collar; prof, Professional, Managerial, and Technical; wc, White Collar.
 
 
-## Funcion if/else
-Prestige$prestige.2 <- ifelse(Prestige$prestige > mean(Prestige$prestige), "alto","bajo")
+## If/else function.
+Prestige$prestige.2 <- ifelse(Prestige$prestige > mean(Prestige$prestige), "high","low")
 
-## Etiquetas:
-is(Prestige$type) # que es?
+## Tags:
+is(Prestige$type) # "what is"?
 
-levels(Prestige$type) # que etiquetas tiene?
+levels(Prestige$type) # What labels does it have?
 
-## Recodificar: de ENG a ESP
+## Recode:
 install.packages("plyr") 
 library(plyr)
-Prestige$type.2 <- revalue(Prestige$type, c("bc"="Tecnico", "prof"="Profesional", "wc"="Oficinista"))
+Prestige$type.2 <- revalue(Prestige$type, c("bc"="Technical", "prof"="Professional", "wc"="Office Job"))
