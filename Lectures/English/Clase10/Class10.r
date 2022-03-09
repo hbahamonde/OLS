7ff7b3cb9c1e83fa475110ad05d1fe4fc0df3178
Hector <bahamondeh@gmail.com>
2022-03-09 15:01:46 +0200

    translation

--- Lectures/English/Clase10/Class10.r
+++ Lectures/English/Clase10/Class10.r
@@ -42,7 +42,7 @@ graphics.off()
 # Thus, the real equation is rather this one:
 # Prestige_{i} = b0 + b1*Schooling_{i} + e_{i}
 # where "i" in this case means "individual."
-# Thus, every "i" has its own prestige and schooling levels (as well as the "e"rror level...we'll talk about that later...)
+# Thus, every "i" has its own prestige and schooling levels (as well as the "e"rror level...we'll talk about that later...).
 
 # So, this means we've got the following...
 
@@ -52,137 +52,135 @@ graphics.off()
 ## B        5                 10            1         2         ?
 ## C        2                 8             1         2         ?
 
-## Recuerda (1): son los mismos betas para todos.
-## Recuerda (2): Por ej., para el caso de Pedro, la formula 
-## "Prestigio = b0 + b1Educacion + e", 
-## o "Prestigio_{i} = b0 + b1Educacion_{i} + e_{i}", que es lo mismo, 
-## seria asi:
+# Remember (1): it's the same beta (0 and 1) for everyone.
+# Remember (2): For person A, the formula 
+# "Prestige = b0 + b1Schooling + e", 
+# o "Prestige_{i} = b0 + b1Schooling_{i} + e_{i}", which is the same, 
+# would be like this for person A:
 
-## Pedro: 
-## 15 = 1 + 2*10 - 6
-
-######################################################################
-## (1)
-## Como seria para el caso de Juan y Diego?
-## Responde
-######################################################################
-
-## Juan?
-
-
-## Diego?
+# Person A: 
+# 15 = 1 + 2*10 - 6
 
 ######################################################################
-## OK; sigamos. Ahora carguemos una base de datos real.
+# How would it be for Person B and Person C?
+# Answer.
+######################################################################
+
+
+## Person B?
+
+
+## Person C?
+
+
+######################################################################
+## OK; let's continue. Now let's load a real dataset.
 
 if (!require("pacman")) install.packages("pacman"); library(pacman)
 p_load(car) # En este paquete, "vive" la base "Prestige" ("Prestigio").
 
-# Carga la base
+# load the dataset
 data(Prestige)
 
-# Mira como se ve la base de datos
+# check the dataset 
 head(Prestige)
 
-# Extrae la 'x' para conseguir beta1
+# Note that all betas are calculated as a lineal combination of the X's and the Y.
+# Now, let's extract 'x' to get beta1 
 x = Prestige$education
 
-# Ve como se ve "x"
+# Let's see how "x" looks like
 x
 
+# Remember that to get beta0 you need to add a vector with as many 1's as the length of the dataset. 
+# Just like we did with our Person A, B, and C dataset.
+# That's why many statisticians call it "the constant" (because it's just a constant, not a *variable*).
 
-## Recuerda que para conseguir beta0 debes anexar una columna de 1's.
+# Let's tell R to repeat as many 1's as the length of our dataset.
+ones = rep(1,length(x)) 
 
-## Aqui le decimos a R que repita 1s, del largo (o "lenght") de 'x'.
-unos = rep(1,length(x)) 
+# Let's check
+ones
 
-# Ve como se ve "unos"
-unos
-
-# Ahora junta ambas cosas ("x" y "unos")
+# Now let's combine the "x" and "ones" objects
 
 x = matrix(
-  c(unos,x), # meter vectores 'unos' y 'x'
-  ncol = 2 # y organizarlos en 2 columnas.
+  c(unos,x), # plug in 'ones' and 'x'
+  ncol = 2 # organize these two guys in 2 columns.
   )
 
-x # mira como se ve la nueva matrix "x"
+x # Let's inspect the X matrix
             
-# Extrae la 'y'
+# Now let's extract the Y vector
 y = matrix(Prestige$prestige)
 
+# And inspect it...
 y
 
 ######################################################################
-## (2)
-## Calcula beta0 y beta1
+# Let's calculate beta0 and beta1
 ######################################################################
 
-## Recuerda los operadores
+# Just to remind you about the operators...
 
-# Transponer 
+# To transpose, use:
 # t()
 
-# Invertir
+# To inver, use:
 # inv()
-## Recuerda cargar "matlib" para usar la funcion "inv()".
+
+# Also, load the matlib library to use the  "inv()" function.
 if (!require("pacman")) install.packages("pacman"); library(pacman)
 p_load(matlib)
 
-# Multiplicar matrices
-# Por ejemplo x*x es  x%*%x
+# And to multiply matrices with "matlib", use the "%" operators, like so...
+# x*x needs to be entered as  x%*%x
 
-# OK; ahora tu. Calcula b = (x'x)^-1x'y
+# OK; not it's your turn. 
+# Calculate b = (x'x)^-1x'y
 
 ######################################################################
-# Problema
+# Issues
 ######################################################################
 
-# Funciona
+# This works...
 inv(t(x) %*% x) %*% t(x) %*% y
 
-# No Funciona
-inv(x) # Matrix debe ser cuadrada, pero "x" es rectancular.
+# This doesn't...
+inv(x) 
 
-# Por que?
+# Why?
 
-inv(t(x) %*% x) # funciona
-# Porque para multiplicar una matriz, debe ser CONFORMABLE.
-# La dimension de "x" es 2x102 (dos columnas, 102 filas). Si "x" fuera 2x2, se podria.
-# Sin embargo, al multiplicar x' (que es 102x2) y x (que es 2x102), por 
-# el "inner rule" (no se el espanol), es decir, el 2 y el otro 2, 
-# sabemos que la invertidad de x'x esta definida, porque x'x es cuadrado.
-
-
-######################################################################
-## (3) 
-## Comprueba que hiciste todo bien.
-## Estima una regresion lineal, pero ocupando
-## el comando "lm" (por "linear model"). El comando "lm" se ocupa asi:
-## lm(y ~ x). Define 'y' como 'Prestige$prestige' y 'x' como 'Prestige$education'.
-
-modelo = lm(Prestige$prestige ~ Prestige$education)
-
-options(scipen=9999)
-summary(modelo)
-
-######################################################################
-## (4) Usando el comando 'plot', haz un grafico entre Prestigio y Educacion.
-data(Prestige)
-
+inv(t(x) %*% x) # This works...
+# This is because, matrices need to be conformable.
+# This means that the inner dimensions of the X matrix (2x102, columns/rows) and the
+# dimensions of the X' (transposed!) vector is (102x2, columns/rows) DO coincide
+# according to the "inner rule". 
 
 
 ######################################################################
-## (5) Usando tus propias palabras, y usando los resultados 
-## de tu regresion lineal y el grafico, interpreta que significa
-## beta0 y que significa beta1.
+# Let's check your work...
+# Estimate a lineal regression model but using "lm" 
+# You have to use "lm" with the following arguments:
+# lm(y ~ x), where Y = Prestige$prestige and X = Prestige$education
 
+model = lm(prestige ~ education, data = Prestige)
 
+options(scipen=9999) # turns off scientific notation
+summary(model) # summarizes the output of the model
 
-## (5.1) Que significa beta0? Responde.
+# Let's do also a little plot.
+p_load(sjPlot)
+plot_model(model, type = "pred", terms = "education")
 
+######################################################################
+# VERY NICE! Bravo! But...what does it mean!?
+######################################################################
 
+# Use your own words to describe the results...
+# Make sure look at the summary() and the plot.
+# What do beta0 ("Intercept") and beta1 (education) mean?
+# Answer...
 
-## (5.2) Que significa beta1? Responde.
 
 
