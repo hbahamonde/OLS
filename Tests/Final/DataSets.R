0ec745efdf05b5c3bf5f0c671e8cff27dbfe6e16
Hector <bahamondeh@gmail.com>
2022-03-10 14:36:06 +0200

    WIP

---
+++ Tests/Final/DataSets.R
@@ -0,0 +1,9 @@
+# install.packages("AER")
+library(AER)
+
+
+data("Fatalities") # "fatal" // numeric. Number of vehicle fatalities.
+data("HousePrices") # "price" // Sale price of a house.
+data("Journals") # "price" // Library subscription price.
+data("Guns") # "violent" // violent crime rate (incidents per 100,000 members of the population).
+data("TeachingRatings") # b"eauty" // rating of the instructor's physical appearance by a panel of six students, averaged across the six panelists, shifted to have a mean of zero.
